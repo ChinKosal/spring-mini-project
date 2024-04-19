44b735fd324d2540067573dfb534b307cb7fcdba
An.Sovathna <ansovathna12@gmail.com>
2024-04-19 21:48:59 +0700

    v7

--- src/main/java/com/example/minispring/repository/CategoryRepository.java
+++ src/main/java/com/example/minispring/repository/CategoryRepository.java
@@ -24,7 +24,7 @@ public interface CategoryRepository {
         @Result(property = "userId",column = "user_id",one = @One(select = "com.example.minispring.repository.AppUserRepository.findUserById"))
     })
     @Select("""
-        select * from categories_tb inner JOIN users_tb on categories_tb.user_id =users_tb.user_id where users_tb.email = #{email} limit #{limit} offset #{offset}
+        select * from categories_tb inner JOIN users_tb on categories_tb.user_id =users_tb.user_id where users_tb.email = #{email} order by categories_tb.category_id asc limit #{limit} offset #{offset}
     """)
     List<Category> getAllCategory(String email,Integer offset,Integer limit);
 
